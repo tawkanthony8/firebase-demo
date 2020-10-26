b636e1959ad6eb20306cd3e66bf0f7af7814f637
jeanmicheltawk <jean-michel.tawk@greybell.com>
2020-10-26 12:11:00 +0200

    addung the first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FirebasedemoComponent } from './component/firebasedemo/firebasedemo.component';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FirebasedemoComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+firebaseConfig : {
+    apiKey: 'AIzaSyDeWtx5KcQMpSRT6c8lZL9r3bGtMC7obIk',
+    authDomain: 'class-demo-2697c.firebaseapp.com',
+    databaseURL: 'https://class-demo-2697c.firebaseio.com',
+    projectId: 'class-demo-2697c',
+    storageBucket: 'class-demo-2697c.appspot.com',
+    messagingSenderId: '666665482437',
+    appId: '1:666665482437:web:530bfc1d577428f06b18ac'
+  }
+  };
