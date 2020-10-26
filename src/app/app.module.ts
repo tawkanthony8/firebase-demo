be62da15048336f709692e98e6846195bcd73382
jeanmicheltawk <jean-michel.tawk@greybell.com>
2020-10-26 15:13:09 +0200

    jm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,7 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig, 'mytestapp'),
+    AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule, // Only required for database features
     AngularFireAuthModule, // Only required for auth features,
     AngularFireStorageModule // Only required for storage features
